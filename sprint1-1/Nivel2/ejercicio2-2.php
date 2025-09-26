6fb37d24f9a642b01995cc10e1deccc45fe0c539
mikelji97 <mikel.falcesino@gmail.com>
2025-09-27 00:55:06 +0200

    Niveles 2

--- sprint1-1/Nivel2/ejercicio2-2.php
+++ sprint1-1/Nivel2/ejercicio2-2.php
@@ -1,2 +1,14 @@
 <?php
 
+class CandyShop
+{
+
+    public function candys(int $chocolates, int $chicles, int $caramelos): int
+    {
+        return ($chocolates * 1) + ($chicles * 0.5) + ($caramelos * 1.5);
+    }
+}
+
+$pedido = new CandyShop();
+
+echo "El total de la compra ha sido: " . $pedido->candys(2,8,6) . " euros.";
